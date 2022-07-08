494630b9a91867f5b780d8b5dff1f0f7bc2489ff
Mario Armenta <89166862+MarioArm1@users.noreply.github.com>
2022-07-08 17:52:21 -0500

    Fix initial test to be able to run spring boot application

---
+++ src/test/java/com/example/clip/AppTest.java
@@ -0,0 +1,14 @@
+package com.example.clip;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void testNothing() {
+        Assertions.assertTrue(true);
+    }
+}
